3f9cc41a1666d4227cd9a65a43f554ac2255e0b7
AKP-13 <alexpeirson@hotmail.com>
2020-09-25 14:22:13 +0100

    Add datefield input to Mood table

--- backend/mood_tracker_api/moods/serializers.py
+++ backend/mood_tracker_api/moods/serializers.py
@@ -4,6 +4,6 @@ from .models import Mood
 class MoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mood
-        fields = ('id', 'moodlevel')
+        fields = ('id', 'date','moodlevel')
 
 

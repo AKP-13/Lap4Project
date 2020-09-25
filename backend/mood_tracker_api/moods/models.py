e645059762daf97c289b30a52c2aa5c6d08f4291
AKP-13 <alexpeirson@hotmail.com>
2020-09-25 13:52:26 +0100

    add min and max validators to moodlevel

--- backend/mood_tracker_api/moods/models.py
+++ backend/mood_tracker_api/moods/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Mood(models.Model):
 
-    moodlevel = models.IntegerField(null=False)
+    moodlevel = models.PositiveIntegerField(default=4, null=False, validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     def __str__(self):
         return f'{self.moodlevel}'

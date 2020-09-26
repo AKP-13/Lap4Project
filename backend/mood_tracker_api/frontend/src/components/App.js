8787f49af4927148abb979b13d0db490b71770c7
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-09-26 17:35:51 +0100

    add Header component and include in Apps.js

--- backend/mood_tracker_api/frontend/src/components/App.js
+++ backend/mood_tracker_api/frontend/src/components/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import ReactDOM from 'react-dom';
+
+// components
+import Header from './layout/Header'
 
 class App extends Component {
-
-
   render() {
     return (
         <div>
-            <h1>Hey I Am rendedffdred</h1>
+            <Header />
+            <h1>Hey I Am rendering</h1>
         </div>
-            
     );
   }
 }
 
 export default App;
 
-const container = document.getElementById("app");
-render(<App />, container);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ backend/mood_tracker_api/frontend/src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Tracker</li>
+                        <li>Journal</li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header

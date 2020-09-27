a08a6f78c9dd7a4bfa14143930c16975302354ae
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-09-27 17:07:36 +0100

    render moods data to page and map over all moods

--- backend/mood_tracker_api/frontend/src/components/moodtracker/Moods.js
+++ backend/mood_tracker_api/frontend/src/components/moodtracker/Moods.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getMoods } from "../../actions/moods";
@@ -8,11 +8,27 @@ class Moods extends Component {
     moods: PropTypes.array.isRequired,
   };
 
+  // to get the Moods before the component mounts
+  componentDidMount() {
+    this.props.getMoods();
+    console.log("component Did Mount");
+  }
+
   render() {
+    console.log(this.props.moods);
     return (
-      <div>
-        <h1>Moods list</h1>
-      </div>
+      <Fragment>
+        <h2>Moods:</h2>
+        {this.props.moods.map((moods) => {
+          console.log(moods);
+          return (
+            <h2 key={moods.id}>
+              Mood Id: {moods.id} | Mood Date: {moods.date} | Mood Level:
+              {moods.moodlevel}
+            </h2>
+          );
+        })}
+      </Fragment>
     );
   }
 }
@@ -21,4 +37,4 @@ const mapStateToProps = (state) => ({
   moods: state.moods.moods, //we wants the moods reducer and the moods within that! moods=[] now we have a prop called moods
 });
 
-export default connect(mapStateToProps)(Moods);
+export default connect(mapStateToProps, { getMoods })(Moods);

ff35c30cff4227b5fda06760aa9210ba687c78fb
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-09-28 15:36:20 +0100

    make the knox token to work and destroy once logged out

--- backend/mood_tracker_api/mood_tracker_api/urls.py
+++ backend/mood_tracker_api/mood_tracker_api/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('', include('frontend.urls')),
     # path('admin/', admin.site.urls),
     path('api/', include('moods.urls')), #allows you to include moods apps urls file
-    path('', include('users.urls')),
+    path('api/', include('users.urls')),
     # path('signup/', user_views.signup, name='signup'),
     # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
--- backend/mood_tracker_api/users/api.py
+++ backend/mood_tracker_api/users/api.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 # we use knox for Tokens
 from knox.models import AuthToken
-from .serializers import UserSerializer, RegisterSerializer
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 # Register API
 class RegisterAPI(generics.GenericAPIView):
@@ -18,5 +18,24 @@ class RegisterAPI(generics.GenericAPIView):
         })
 
 # Login API
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)#to send back errors
+        user = serializer.validated_data
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data, 
+            "token": AuthToken.objects.create(user)[1] #creates unique token for user (knox)
+        })
 
 # Get User API
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user

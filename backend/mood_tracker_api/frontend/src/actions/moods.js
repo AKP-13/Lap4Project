02460942097f49e8c42938298176f76b54cfef5f
AKP-13 <alexpeirson@hotmail.com>
2020-09-28 21:38:20 +0100

    Finish authentication BUT need to only load the logged in user's moods, NOT ALL

--- backend/mood_tracker_api/frontend/src/actions/moods.js
+++ backend/mood_tracker_api/frontend/src/actions/moods.js
@@ -1,43 +1,44 @@
 import axios from "axios";
 import { createMessage, returnErrors } from "./messages";
+import { tokenConfig } from "./auth";
 
 import { GET_MOODS, DELETE_MOOD, ADD_MOOD } from "./types";
 
 // GET MOODS
-export const getMoods = () => (dispatch) => {
-  // make fetch request to localhost:8000/api/
-  axios
-    .get("http://localhost:8000/api/")
-    .then((res) => {
-      dispatch({ type: GET_MOODS, payload: res.data });
-    })
-    .catch((err) =>
-      dispatch(returnErrors(err.response.data, err.response.status))
-    );
+export const getMoods = () => (dispatch, getState) => {
+    // make fetch request to localhost:8000/api/
+    axios
+        .get("http://localhost:8000/api/", tokenConfig(getState))
+        .then((res) => {
+            dispatch({ type: GET_MOODS, payload: res.data });
+        })
+        .catch((err) =>
+            dispatch(returnErrors(err.response.data, err.response.status))
+        );
 };
 
 // DELETE MOOD
-export const deleteMood = (id) => (dispatch) => {
-  // make fetch request to localhost:8000/api/
-  axios
-    .delete(`http://localhost:8000/api/${id}`)
-    .then((res) => {
-      dispatch(createMessage({ deleteMood: "Mood Deleted" }));
-      dispatch({ type: DELETE_MOOD, payload: id });
-    })
-    .catch((err) => console.log(err));
+export const deleteMood = (id) => (dispatch, getState) => {
+    // make fetch request to localhost:8000/api/
+    axios
+        .delete(`http://localhost:8000/api/${id}`, tokenConfig(getState))
+        .then((res) => {
+            dispatch(createMessage({ deleteMood: "Mood Deleted" }));
+            dispatch({ type: DELETE_MOOD, payload: id });
+        })
+        .catch((err) => console.log(err));
 };
 
 // ADD MOOD
-export const addMood = (mood) => (dispatch) => {
-  // make fetch request to localhost:8000/api/
-  axios
-    .post("http://localhost:8000/api/", mood)
-    .then((res) => {
-      dispatch(createMessage({ addMood: "Mood Added" }));
-      dispatch({ type: ADD_MOOD, payload: res.data });
-    })
-    .catch((err) =>
-      dispatch(returnErrors(err.response.data, err.response.status))
-    );
+export const addMood = (mood) => (dispatch, getState) => {
+    // make fetch request to localhost:8000/api/
+    axios
+        .post("http://localhost:8000/api/", mood, tokenConfig(getState))
+        .then((res) => {
+            dispatch(createMessage({ addMood: "Mood Added" }));
+            dispatch({ type: ADD_MOOD, payload: res.data });
+        })
+        .catch((err) =>
+            dispatch(returnErrors(err.response.data, err.response.status))
+        );
 };

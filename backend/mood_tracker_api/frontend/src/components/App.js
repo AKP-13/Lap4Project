ec44675f920379e2330f007120e994ced7f95f93
cindywfeng <cindy.feng.18@ucl.ac.uk>
2020-09-28 11:41:11 +0100

    make alert function work in component did mount

--- backend/mood_tracker_api/frontend/src/components/App.js
+++ backend/mood_tracker_api/frontend/src/components/App.js
@@ -2,9 +2,19 @@ import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
 import ReactDOM from "react-dom";
 
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+//Alert Options
+const alertOptions = {
+  timeout: 3000,
+  position: "top center",
+};
+
 // components
 import Header from "./layout/Header";
 import Dashboard from "./moodtracker/Dashboard";
+import Alerts from "./layout/Alerts";
 
 // redux store
 import { Provider } from "react-redux";
@@ -14,10 +24,13 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <Dashboard />
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <Alerts />
+            <Dashboard />
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }

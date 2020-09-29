393a467f4244660bb1eca7a2b2f7b58d1ca78e02
AKP-13 <alexpeirson@hotmail.com>
2020-09-29 10:28:15 +0100

    Full secure functionlity, users can only see their own moods upon login to dashboard

--- backend/mood_tracker_api/frontend/src/components/moodtracker/Moods.js
+++ backend/mood_tracker_api/frontend/src/components/moodtracker/Moods.js
@@ -4,46 +4,52 @@ import PropTypes from "prop-types";
 import { getMoods, deleteMood } from "../../actions/moods";
 
 class Moods extends Component {
-  static propTypes = {
-    moods: PropTypes.array.isRequired,
-    getMoods: PropTypes.func.isRequired,
-    deleteMood: PropTypes.func.isRequired,
-  };
+    static propTypes = {
+        moods: PropTypes.array.isRequired,
+        getMoods: PropTypes.func.isRequired,
+        deleteMood: PropTypes.func.isRequired,
+    };
 
-  // to get the Moods before the component mounts
-  componentDidMount() {
-    this.props.getMoods();
-    console.log("component Did Mount");
-  }
+    // to get the Moods before the component mounts
+    componentDidMount() {
+        this.props.getMoods();
+        console.log("component Did Mount");
+    }
 
-  render() {
-    console.log(this.props.moods);
-    return (
-      <Fragment>
-        <h2>Moods:</h2>
-        {/* map over moods */}
-        {this.props.moods.map((mood) => {
-          return (
-            <Fragment key={mood.id}>
-              <h2>
-                Mood Id: {mood.id} | Mood Date: {mood.date} | Mood Level:
-                {mood.moodlevel} | User-id: {mood.user}
-              </h2>
-              {/* delete button */}
-              <button onClick={this.props.deleteMood.bind(this, mood.id)}>
-                Delete
-              </button>
+    render() {
+        console.log(this.props.moods);
+        return (
+            <Fragment>
+                <h2>Moods:</h2>
+                {/* map over moods */}
+                {this.props.moods.map((mood) => {
+                    return (
+                        <Fragment key={mood.id}>
+                            <h2>
+                                Mood Id: {mood.id} | Mood Date: {mood.date} |
+                                Mood Level:
+                                {mood.moodlevel}
+                            </h2>
+                            {/* delete button */}
+                            <button
+                                onClick={this.props.deleteMood.bind(
+                                    this,
+                                    mood.id
+                                )}
+                            >
+                                Delete
+                            </button>
+                        </Fragment>
+                    );
+                })}
+                {/* end of map over moods */}
             </Fragment>
-          );
-        })}
-        {/* end of map over moods */}
-      </Fragment>
-    );
-  }
+        );
+    }
 }
 
 const mapStateToProps = (state) => ({
-  moods: state.moods.moods, //we wants the moods reducer and the moods within that! moods=[] now we have a prop called moods
+    moods: state.moods.moods, //we wants the moods reducer and the moods within that! moods=[] now we have a prop called moods
 });
 
 export default connect(mapStateToProps, { getMoods, deleteMood })(Moods);

f31615ae223057aea264c53ca9da0d88851b87d1
AKP-13 <alexpeirson@hotmail.com>
2020-09-29 18:11:16 +0100

    Update README and edit model

--- backend/mood_tracker_api/moods/models.py
+++ backend/mood_tracker_api/moods/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Mood(models.Model):
-    date = models.DateField(default=date.today, unique=True)
+    date = models.DateField(default=date.today)
     moodlevel = models.PositiveIntegerField(null=False, validators=[MinValueValidator(1), MaxValueValidator(5)])
     owner = models.ForeignKey(User, related_name="moods", on_delete=models.CASCADE, null=True)
     exerciseQuality = models.PositiveIntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(3)])
@@ -14,8 +14,7 @@ class Mood(models.Model):
     dietQuality = models.PositiveIntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(3)])
     notes = models.CharField(max_length=200, blank=True, null=True)
     
-  
-    
+     
     #name our keys to something readable
     def __str__(self):
         return f'{self.moodlevel} {self.date}'

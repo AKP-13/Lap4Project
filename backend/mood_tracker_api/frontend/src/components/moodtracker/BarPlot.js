3161b5cdfd9afc3578962d2cfaee6ee2ef613d36
AKP-13 <alexpeirson@hotmail.com>
2020-09-30 11:59:23 +0100

    Add basic form to journal, console.log 'submit', on submit

--- backend/mood_tracker_api/frontend/src/components/moodtracker/BarPlot.js
+++ backend/mood_tracker_api/frontend/src/components/moodtracker/BarPlot.js
@@ -16,49 +16,9 @@ class BarPlot extends Component {
     }
 
     render() {
-        let exerciseL = this.props.moods.map((mood) => mood.exerciseQuality);
-        let dietL = this.props.moods.map((mood) => mood.dietQuality);
-        let sleepL = this.props.moods.map((mood) => mood.sleepQuality);
-        let moodlevels = this.props.moods.map((mood) => mood.moodlevel);
-        //fget indices of good moods only
-        let idxs = [];
-        for (i = 0; i <= moodlevels.length - 1; i++) {
-            if (moodlevels[i] == 5 || moodlevels[i] == 4) {
-                idxs.push(i);
-            }
-        }
-        //filter only those indices function
-        function filterArr(oldarr, newarr) {
-            for (idx of idxs) {
-                newarr.push(oldarr[idx]);
-            }
-        }
-        //invoke
-        let filtereddiet = [];
-        let filteredexercise = [];
-        let filteredsleep = [];
-        filterArr(dietL, filtereddiet);
-        filterArr(sleepL, filteredsleep);
-        filterArr(exerciseL, filteredexercise);
-        //get average
-        const arrAvg = (arr) => arr.reduce((a, b) => a + b, 0) / arr.length;
-        let sleepavg = arrAvg(filteredsleep);
-        let exerciseavg = arrAvg(filteredexercise);
-        let dietavg = arrAvg(filtereddiet);
-
-        console.log(exerciseL);
-        // const data = [
-        //     { text: "exercise", value: exerciseavg },
-        //     { text: "diet", value: dietavg },
-        //     { text: "sleep", value: sleepavg },
-        // ];
-
         return (
             <div>
-                <h4>
-                    Your mood was at its best when you rated your habits as
-                    follows:
-                </h4>
+                <h4>BAR PLOT WILL GO HERE</h4>
             </div>
         );
     }
--- backend/mood_tracker_api/frontend/src/components/moodtracker/Journal.js
+++ backend/mood_tracker_api/frontend/src/components/moodtracker/Journal.js
@@ -1,14 +1,42 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getMoods, deleteMood } from "../../actions/moods";
 
 export class Journal extends Component {
+    state = {
+        journalEntry: "",
+    };
+
     static propTypes = {};
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log("You clicked submit!");
+    };
+
     render() {
+        const { journalEntry } = this.state;
+
         return (
             <div>
                 <h1>Journal</h1>
+                <form id="journalForm" onSubmit={this.onSubmit}>
+                    <label htmlFor="journal">Add a jounral entry:</label>
+
+                    <textarea
+                        id="journal"
+                        name="journalEntry"
+                        rows="5"
+                        cols="33"
+                        onChange={this.onChange}
+                        value={journalEntry}
+                        placeholder="Thoughts for the day..."
+                    ></textarea>
+                    <input type="submit"></input>
+                </form>
             </div>
         );
     }
